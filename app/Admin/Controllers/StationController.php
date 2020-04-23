e0a6d7a75709e5c23223c0eceb590488f3cc1957
xa45 <xa45@ms.gov>
2020-04-23 15:11:07 +0800

    modify models

--- app/Admin/Controllers/StationController.php
+++ app/Admin/Controllers/StationController.php
@@ -62,13 +62,18 @@ class StationController extends AdminController
       $show = new Show(Station::findOrFail($id));
 
       $show->field('id', __('Id'));
-      $show->field('area_id', __('Area id'));
+
+      //ref https://github.com/z-song/laravel-admin/issues/3107
+      $show->area_id('地區名稱')->as(function ($area_id) {
+         return Area::where('id', $area_id)->first()->area_name ?? null;
+      });
+
       $show->field('station_code', __('測站代號'));
       $show->field('station_name', __('測站名稱'));
       $show->field('address', __('地址'));
       $show->field('telno', __('電話'));
       $show->field('remark', __('備註'));
-      $show->field('footer', __('網站資訊'));
+      $show->field('footer', __('網站資訊footer'))->unescape();
       $show->field('order', __('排序'));
       $show->field('valid_at', __('有效日期'));
       $show->field('mod_user', __('異動人員'));
@@ -92,7 +97,7 @@ class StationController extends AdminController
       $form->text('station_name', __('測站名稱'))->rules('required|max:40');
       $form->text('address', __('地址'));
       $form->text('telno', __('電話'));
-      $form->ckeditor('footer', __('網站資訊'));
+      $form->ckeditor('footer', __('網站資訊footer'));
       $form->text('order', __('排序'));
       $form->datetime('valid_at', __('有效日期'))->default(date('Y-m-d H:i:s'));
       $form->textarea('remark', __('備註'));
